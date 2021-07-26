e0d9a99287adae57e675fe382fee7f7ed9723e4e
QuickStrike123 <73514740+QuickStrike123@users.noreply.github.com>
2021-07-26 10:32:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function AddPlayer(){
+
+    Player1 = document.getElementById("Player1Name").value;
+
+    Player2 = document.getElementById("Player2Name").value;
+
+    localStorage.setItem("Player1",Player1);
+
+    localStorage.setItem("Player2",Player2);
+
+    window.location = "index2.html";
+
+}
